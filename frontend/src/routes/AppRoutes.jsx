9017f9a01606bd5a5e826ba565cd6e188bf7e9a8
SunayKulkarni <sunaykulkarni17@gmail.com>
2025-05-08 18:26:25 +0000

    Fix:19

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ const AppRouter = () => {
     <BrowserRouter>
         <Routes>
             <Route path="/" element={ <UserAuth><Home/></UserAuth> } />
+            <Route path="/projects/all" element={ <UserAuth><Home/></UserAuth> } />
             <Route path="/login" element={ <Login/> } />
             <Route path="/register" element={ <Register/> } />
             <Route path="/project" element={ <UserAuth><Project/></UserAuth> } />
