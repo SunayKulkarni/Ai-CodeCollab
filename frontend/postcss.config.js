5d821a03c1ef068ae9d5432d82f5a1d6122489e1
SunayKulkarni <sunaykulkarni17@gmail.com>
2025-05-08 14:11:39 +0000

    Fix 6

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': {},
-    '@tailwindcss/postcss': {},  // 👈 use the new PostCSS plugin
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
